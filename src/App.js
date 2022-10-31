5e027387350eed60530c3d0040afef9578078c98
ewangelia <114274943+ewangelia@users.noreply.github.com>
2022-11-01 00:03:44 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import Footer from './footer.js';
+import './footer.css';
+import Tabel from './tabel.js';
+import './tabel.css';
+import Title from './Title.js';
+import './Title.css';
+
+
+function App() {
+  return (
+    <div>
+      <Title/>
+      <Tabel/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
